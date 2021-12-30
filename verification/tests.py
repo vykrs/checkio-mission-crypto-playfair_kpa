fb62f445e4344c9075913590235bfc5fdb449e8d
vykrs <59094544+vykrs@users.noreply.github.com>
2021-12-30 18:55:45 +0300

    first upload

---
+++ verification/tests.py
@@ -0,0 +1,154 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    'Basic': [
+            {
+                'input': ['pythonsxstandardlibraryisveryextensiveofferingawiderangeofstuffx',
+                          'aiwblarskwphydowzehmhoieksxlixgwvufxlvzqvizxbehdycxlphyxzqkwcvsi'],
+                'answer': 'dmhfiulxgbxvqhyx'
+            },
+            {
+                'input': ['sixcrazykingsvowedtoabolishmyquitepitifulioust',
+                          'zlgrcekqztvoolunhbvkemsvlzadnpzflrqlvlhwtluzkl'],
+                'answer': 'vklprhcrixbpzebc'
+            },
+            {
+                'input': ['zombiesquicklyatetwelveofmyneighboursx',
+                          'uzuywyksmzdcvhfgtnftonbnkfevywlgxzmxzd'],
+                'answer': 'xnzpchtyrfcwpkth'
+            }
+    ],
+    'Extra': [
+            {
+                'input': ['youshouldreadeliezeryudkowskysrationalityfromaitozombies',
+                          'kcohsuwcxabmvawqmpbefcxflooxkgemztcxeutzfikrrmtzwswrmyrp'],
+                'answer': 'xwgpblebzirphrpb'
+            },
+            {
+                'input': ['thexexampleaboveisaverysimplequantummechanicsequationx',
+                          'bghyhyganmgvrhefzivsoklvaxnmofgspwglygxospzxvunogmzhdl'],
+                'answer': 'rgnaoykomauviveu'
+            },
+            {
+                'input': ['echoofaclockrhythmicallypulsatedinmyveinsx',
+                          'xepkvuuyctxopnlmrkgybyycocxdbmlwdrirwzdrqh'],
+                'answer': 'mvhgxezymoxwiqwc'
+            },
+            {
+                'input': ['wearealxlfromxanthcubesaidquicklyiustvisitingphaze',
+                          'baixamwkblsfasihxatgcbxmmungeuqgnmdrgamrauficqawpb'],
+                'answer': 'dwyzbzzidamzxmcv'
+            },
+            {
+                'input': ['youshouldreadeliezeryudkowskysrationalityfromaitozombies',
+                          'kcohsuwcxabmvawqmpbefcxflooxkgemztcxeutzfikrrmtzwswrmyrp'],
+                'answer': 'xwgpblebzirphrpb'
+            },
+            {
+                'input': ['aquickbrownfoxiumpsoverthelazydogx',
+                          'quqdyngvshtchpdqxoofryfqcwbtbwmspq'],
+                'answer': 'xfzowyeixnwrvuri'
+            },
+            {
+                'input': ['doesthetreemakeasoundifitfallsintheforestwherenobody',
+                          'lccwbsawmzvwgtozhcnysduxhnokdhgubsopzlcwbhwomzfahzru'],
+                'answer': 'hauwoamzwkcwtcva'
+            },
+            {
+                'input': ['discoelysiumanisometricdetectiverpgwhatkindofcopareyou',
+                          'lzhxcdasilfnpulimwaqtzoeaqcfqzhppkmsyerbrgomhfkdptahbm'],
+                'answer': 'zblvcfqpzulhylfl'
+            },
+            {
+                'input': ['ideaofcognitivebiasinpsychologyworksinanalogousway',
+                          'eobdkqiwbtrxpxbaelxcvecwpyxomqwsmiwvexdeuymqmlcsuh'],
+                'answer': 'xmcvrpirrtcnnybr'
+            }
+    ],
+    'Special': [
+            {
+                'input': ['abacadaebcbdbecdcedeafbfcfdfefagbg',
+                          'wgqlpuslimtzomryemkrwhfmmvzxmnghti'],
+                'answer': 'gbawmelmbrkowqol'
+            },
+            {
+                'input': ['thefiveboxingwizardsiumpquicklyx',
+                          'irfbnhfgisotcpnxkaolxizaudxudruz'],
+                'answer': 'enwzigbtmrofmwqf'
+            },
+            {
+                'input': ['abcdefghiklmnopqrstuvwxyzx',
+                          'scxviuklhzhlorqlngoeyxwvdh'],
+                'answer': 'ouygqboqotbttstq'
+            },
+            {
+                'input': ['iackdawslovesmybigblacksphinxofquartzx',
+                          'rckuxihpmkdxohgrbnctcbzoswsdepuxfcbmpv'],
+                'answer': 'mzkoqromhwdopiyv'
+            },
+            {
+                'input': ['fiftysixbigredietplaneszoxomedbythetower',
+                          'krnmpkufruioznuzneatpczhbwraznoqvccnayic'],
+                'answer': 'acskctcivlzpyvez'
+            },
+            {
+                'input': ['letusconfirmaphonydoubtx',
+                          'ufetqvlkpxywgeydkrlqoeiz'],
+                'answer': 'zuvgvtwfcwvaagap'
+            },
+            {
+                'input': ['mzpyfihnfzvmpokuomptmb',
+                          'hqthbemywrxaldcnaztsqx'],
+                'answer': 'gptygklfvhtfxpig'
+            },
+            {
+                'input': ['nxnymfyubalsbvbwckbhoqwbgx',
+                          'mtlxshpdmsrapbzpoumccfpzti'],
+                'answer': 'khibvoevhxrzzskr'
+            },
+            {
+                'input': ['discoelysiumanisometricdetectiverpgwhatkindofcopareyou',
+                          'yuiqrfxluspueqsuuhabospyablnxsebunfdtfhqrcwulkukeslvir'],
+                'answer': 'hsqulnnbmbarqtef'
+            },
+            {
+                'input': ['echoofaclockrhythmicallypulsatedinmyveinsx',
+                          'gbtssogqdmbxirmrtkgpgeamfgeolrlbwckxsawcpk'],
+                'answer': 'lmfvgbhaltbkveua'
+            },
+            {
+                'input': ['ohfukrwsvsbepegqansgzpdmtbdrcv',
+                          'colwsgftzviaiuygevkrpuflbpfymk'],
+                'answer': 'hwtzodywbfwnvwyo'
+            },
+            {
+                'input': ['innitrkmqcmocvvbydmsux',
+                          'hoohwpsralxngeuzhrnelo'],
+                'answer': 'uimfcmmwepcefccv'
+            },
+            {
+                'input': ['rehoomhoonaelkqlndplqedahccggf',
+                          'edsvlqsvbqudzlmocrovmuauvhityx'],
+                'answer': 'wvoxdmedzcukxuza'
+            },
+            {
+                'input': ['thisexamplecontainsunxnesesxsarilylongplaintextandciphertextyouprobablydontnexeda' +
+                          'samplethisbigtobreaktheplayfairencryptionsoadxdsomekindofcontrolwhenthekeytableis' +
+                          'foundyoushouldstopadxdingnewdigraphsanywaytomakesurethatwehavexenoughletxtersabcd' +
+                          'efghiklmnopqrstuvwxyz',
+                          'wgbpomebicytaqueblbsqbdagafbbnxrclqydsicybduomuedlpyhfvmgdmzvacsxvaninelaqudomdtn' +
+                          'bebicdgprbampzemxyedwgvicevsorxadwilicmaqfaenmqfaedlrdlqxzyduxvkcgvdugvdvecnadybp' +
+                          'xqsuleazgfazklguyfenmqblsdvtlmhmysfgnuvcevzebedvbsmvwgeutvsvoymoqatspkdgmzvmbniut' +
+                          'dspprqkbdyfkxguwazroc'],
+                'answer': 'zehgytmvgeagbnmd'
+            }
+    ]
+}
